79eedfa472bcac435f08a066aa4f9f496f91b41c
Laforeze <dylanf1002d@gmail.com>
2023-11-01 12:15:54 -0500

    added files for account list and signup

---
+++ app/src/main/java/com/example/crapmap/accountSignup.java
@@ -0,0 +1,14 @@
+package com.example.crapmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accountSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_signup);
+    }
+}
