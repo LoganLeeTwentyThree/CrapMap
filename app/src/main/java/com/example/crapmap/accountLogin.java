2dcdcf2d30c5ea9622b73eac3f364990ef85efbd
Laforeze <dylanf1002d@gmail.com>
2023-11-06 20:00:08 -0600

    added files for account list and signup

--- app/src/main/java/com/example/crapmap/accountLogin.java
+++ app/src/main/java/com/example/crapmap/accountLogin.java
@@ -1,12 +1,36 @@
 package com.example.crapmap;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import android.os.Bundle;
 
 import com.example.crapmap.R;
 
 public class accountLogin extends AppCompatActivity {
+
+
+    public class UserSelect_Activity {
+
+        // Logic to display the list of users from the user list and allow user selection
+        public void displayUserList(UserList userList) {
+            try {
+                BufferedReader csvReader = new BufferedReader(new FileReader(userList.getCsvFile()));
+                String row;
+                while ((row = csvReader.readLine()) != null) {
+                    String[] data = row.split(",");
+                    // Display the data in the UI or console
+                    System.out.println("Name: " + data[0] + ", ID: " + data[2]);
+                }
+                csvReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/crapmap/accountSignup.java
+++ app/src/main/java/com/example/crapmap/accountSignup.java
@@ -2,6 +2,8 @@ package com.example.crapmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import android.os.Bundle;
 
 public class accountSignup extends AppCompatActivity {
@@ -11,5 +13,25 @@ public class accountSignup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_signup);
 
+
+        public class AddUser_Activity {
+
+            // Logic to take in info from fields and create an entry in the user list CSV
+            public void addUserToCSV(UserList userList, String name, String password, int id) {
+                try {
+                    FileWriter csvWriter = new FileWriter(userList.getCsvFile(), true);
+                    csvWriter.append(name);
+                    csvWriter.append(",");
+                    csvWriter.append(password);
+                    csvWriter.append(",");
+                    csvWriter.append(String.valueOf(id));
+                    csvWriter.append("\n");
+                    csvWriter.flush();
+                    csvWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
