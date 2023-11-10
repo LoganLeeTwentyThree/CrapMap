88a79ecf2980794c115934b0aabc793152d53672
shrdlu <git@rk.blueeyedzoo.com>
2023-11-10 15:27:23 -0600

    added toilet list and user profile activities

---
+++ app/src/main/java/com/example/crapmap/ToiletListActivity.java
@@ -0,0 +1,14 @@
+package com.example.crapmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToiletListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toilet_list);
+    }
+}
