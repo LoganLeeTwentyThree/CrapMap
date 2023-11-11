1aa81243cf901a9495d7c261b67723ae4308eccd
shrdlu <git@rk.blueeyedzoo.com>
2023-11-10 18:10:41 -0600

    worked on toilet list - got buttons working, but not launching activities yet. Also still need to add buttons for each profile

--- app/src/main/java/com/example/crapmap/ToiletListActivity.java
+++ app/src/main/java/com/example/crapmap/ToiletListActivity.java
@@ -3,6 +3,11 @@ package com.example.crapmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ToiletListActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class ToiletListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toilet_list);
+
+        ImageButton back_btn = findViewById(R.id.back_btn);
+        ImageButton plus_btn = findViewById(R.id.plus_btn);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("ToiletListActivity.java", "clicked back button");
+            }
+        });
+
+        plus_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("ToiletListActivity.java", "clicked plus button");
+            }
+        });
+
+        // TODO: find a way to procedurally add buttons to each element in the list, and set it up so they'll launch the respective profiles
     }
 }
