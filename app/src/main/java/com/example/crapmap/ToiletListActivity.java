c6c7667a7d25498588d63e6a95dbe4be4faca3ea
shrdlu <git@rk.blueeyedzoo.com>
2023-11-13 13:22:37 -0600

    mainActivity buttons working (mostly, we need to add profile and map views to fully work), and got toiletlist buttons working

--- app/src/main/java/com/example/crapmap/ToiletListActivity.java
+++ app/src/main/java/com/example/crapmap/ToiletListActivity.java
@@ -2,6 +2,7 @@ package com.example.crapmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,17 +20,23 @@ public class ToiletListActivity extends AppCompatActivity {
         ImageButton back_btn = findViewById(R.id.back_btn);
         ImageButton plus_btn = findViewById(R.id.plus_btn);
 
+        // back button
         back_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d("ToiletListActivity.java", "clicked back button");
+                finish();
             }
         });
 
+        // plus button
         plus_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d("ToiletListActivity.java", "clicked plus button");
+                Intent intent = new Intent(ToiletListActivity.this, AddToiletActivity.class);
+                startActivity(intent);
             }
         });
+
 
         // TODO: find a way to procedurally add buttons to each element in the list, and set it up so they'll launch the respective profiles
     }
