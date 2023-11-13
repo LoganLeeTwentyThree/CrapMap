4c4ccd1bb8b7b8a331e45749afe55df7e3523e5c
Laforeze <dylanf1002d@gmail.com>
2023-11-13 13:22:40 -0600

    added files for account list and signup

--- app/src/main/java/com/example/crapmap/accountLogin.java
+++ app/src/main/java/com/example/crapmap/accountLogin.java
@@ -3,8 +3,13 @@ package com.example.crapmap;
 import androidx.appcompat.app.AppCompatActivity;
 import javax.swing.*;
 import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import com.example.crapmap.R;
@@ -13,29 +18,35 @@ import com.example.crapmap.model.UserList;
 public class accountLogin extends AppCompatActivity {
 
 
-    public class UserSelect_Activity {
+
 
         // Logic to display the list of users from the user list and allow user selection
-        public void displayUserList(UserList userList) {
+        public void displayUserList() {
             try {
-                BufferedReader csvReader = new BufferedReader(new FileReader(userList.getCsvFile()));
-                String row;
-                while ((row = csvReader.readLine()) != null) {
+                UserList userList = new UserList("userList.csv");
+                AssetManager manager = this.getAssets();
+                InputStream csvReader = manager.open(userList.getCsvFile());
+
+                Scanner scanr = new Scanner(csvReader);
+                while ((scanr.hasNext()) ) {
+                    String row = scanr.nextLine();
                     String[] data = row.split(",");
                     // Display the data in the UI or console
-                    System.out.println("Name: " + data[0] + ", ID: " + data[2]);
+                    System.out.println("Name: " + data[0] + ", ID: " + data[1]);
                 }
                 csvReader.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-    }
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_login);
+
+        displayUserList();
     }
 }
